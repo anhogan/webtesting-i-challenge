1a0e9ec632ddabe45582ba368749302469f0a1f7
Amanda Hogan <ahogan@teamworks.com>
2020-03-25 11:49:18 -0400

    Setup tests to be written

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,97 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('Repair Item', () => {
+  it.todo('Resets durability to 100', () => {
+
+  });
+
+  it.todo('Returns item name and enhancement without change', () => {
+
+  });
+
+  it.todo('Throws error if item is not an object', () => {
+
+  });
+
+  it.todo('Throws an error if item does not contain name, durability, or enhancement', () => {
+
+  });
+});
+
+describe('Enhancement Success', () => {
+  it.todo('Adds one to enhancement', () => {
+
+  });
+
+  it.todo('Prevents enhancement from going over 20', () => {
+
+  });
+
+  it.todo('Returns item name and durability without change', () => {
+
+  });
+
+  it.todo('Throws error if item is not an object', () => {
+
+  });
+
+  it.todo('Throws an error if item does not contain name, durability, or enhancement', () => {
+
+  });
+});
+
+describe('Enhancement Failure', () => {
+  it.todo('Decreases durability by 5', () => {
+
+  });
+
+  it.todo('Decreases durability by 10 when enhancement is 15', () => {
+
+  });
+
+  it.todo('Decreases durability by 10 when enhancement is 16 or more', () => {
+
+  });
+
+  it.todo('Decreases enhancement by 1', () => {
+
+  });
+
+  it.todo('Returns item name and enhancement without change', () => {
+
+  });
+
+  it.todo('Returns item name without change', () => {
+
+  });
+
+  it.todo('Throws error if item is not an object', () => {
+
+  });
+
+  it.todo('Throws an error if item does not contain name, durability, or enhancement', () => {
+
+  });
+});
+
+describe('Get Item', () => {
+  it.todo('Returns item without change when enhancement is 0', () => {
+
+  });
+
+  it.todo('Returns item durability and enhancement without change', () => {
+
+  });
+
+  it.todo('Updates item name', () => {
+
+  });
+
+  it.todo('Throws error if item is not an object', () => {
+
+  });
+
+  it.todo('Throws an error if item does not contain name, durability, or enhancement', () => {
+
+  });
+});
